0489f2609320f3e6319c1c2835f5c606ba5bfe48
Tom Rez <rezun79@gmail.com>
2022-05-14 18:11:42 +0200

    Add modules config ver3

--- AZ_Terraform/Modules/RBAC/main.tf
+++ AZ_Terraform/Modules/RBAC/main.tf
@@ -21,19 +21,19 @@ resource "azurerm_role_assignment" "rg_owner" {
   scope                = "/subscriptions/653f9b86-b9d4-4959-abfd-f8a6a6bf9937/resourceGroups/${local.rg_name}"
   role_definition_name = "Owner"
   principal_id         = data.azuread_group.aad_owner_group.id
-  depends_on = [azurerm_resource_group.rg1, module.AAD_Groups.azuread_group.aad_owner_group]
+  # depends_on = [azurerm_resource_group.rg1, module.AAD_Groups.azuread_group.aad_owner_group]
 }
 
 resource "azurerm_role_assignment" "rg_contributor" {
   scope                = "/subscriptions/653f9b86-b9d4-4959-abfd-f8a6a6bf9937/resourceGroups/${var.rg_name}"
   role_definition_name = "Contributor"
   principal_id         = data.azuread_group.aad_contributor_group.id
-  depends_on = [azurerm_resource_group.rg1, module.AAD_Groups.azuread_group.aad_contributor_group]
+  # depends_on = [azurerm_resource_group.rg1, module.AAD_Groups.azuread_group.aad_contributor_group]
 }
 
 resource "azurerm_role_assignment" "rg_reader" {
   scope                = "/subscriptions/653f9b86-b9d4-4959-abfd-f8a6a6bf9937/resourceGroups/${var.rg_name}"
   role_definition_name = "Reader"
   principal_id         = data.azuread_group.aad_reader_group.id
-  depends_on = [azurerm_resource_group.rg, module.AAD_Groups.azuread_group.aad_reader_group]
+  # depends_on = [azurerm_resource_group.rg, module.AAD_Groups.azuread_group.aad_reader_group]
 }
