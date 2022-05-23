05edbc47b84e4dac86d2d45055ad04ea6a84e65a
rezun79 <rezun79@gmail.com>
2022-05-23 08:42:45 +0200

    refactor

--- AZ_Terraform/main.tf
+++ AZ_Terraform/main.tf
@@ -1,6 +1,6 @@
 module "rbac" {
-  #source = "git::https://github.com/Rezun79/tf_mod_rbac.git"
-  source = "../../tf_mod_rbac/"
+  source = "git::https://github.com/Rezun79/tf_mod_rbac.git"
+  #source = "../../tf_mod_rbac/"
   rg_name = var.rg_name
   subscription_id = var.subscription_id
   spn_object_id = module.spn.spn_object_id
@@ -11,14 +11,14 @@ module "rbac" {
 }
 
 module "rg" {
-  #source = "git::https://github.com/Rezun79/tf_mod_rg.git"
-  source = "../../tf_mod_rg/"
+  source = "git::https://github.com/Rezun79/tf_mod_rg.git"
+  #source = "../../tf_mod_rg/"
   rg_name = var.rg_name
 }
 
 module "kayvault" {
-  #source = "git::https://github.com/Rezun79/tf_mod_keyvault.git"
-  source = "../../tf_mod_keyvault/"
+  source = "git::https://github.com/Rezun79/tf_mod_keyvault.git"
+  #source = "../../tf_mod_keyvault/"
   rg_name = var.rg_name
   root_spn_client_id = var.client_id
   spn_client_id = module.spn.spn_client_id
@@ -37,8 +37,8 @@ module "kayvault" {
 }
 
 module "spn" {
-  #source = "git::https://github.com/Rezun79/tf_mod_spn.git"
-  source = "../../tf_mod_spn/"
+  source = "git::https://github.com/Rezun79/tf_mod_spn.git"
+  #source = "../../tf_mod_spn/"
   rg_name = var.rg_name
   # key_vault_name = var.key_vault_name
   # key_vault_rg_name = var.key_vault_rg_name
